b3dc8a7cc7e53ba7d9f94672eda48dd66d35ee76
minheejeon <minhee.jeon@simplekey.co.kr>
2025-12-16 17:38:49 +0900

    SSR(Server Side Rendering) CSR(Client Side Rendering) 실습

--- src/main/java/com/simplekey/alpinespringbootdemo/HelloController.java
+++ src/main/java/com/simplekey/alpinespringbootdemo/HelloController.java
@@ -1,13 +1,25 @@
 package com.simplekey.alpinespringbootdemo;
 
+import com.simplekey.dto.Todo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Todo> javaTodos = new ArrayList<>();
+        javaTodos.add(new Todo("Spring에서 보낸 데이터 1", false));
+        javaTodos.add(new Todo("Alpine.js로 렌더링하기", true));
+        javaTodos.add(new Todo("서버 데이터 연동 성공!", false));
+
+        model.addAttribute("serverTodos",  javaTodos);
+
         return "home";
     }
 }
---
+++ src/main/java/com/simplekey/dto/Todo.java
@@ -0,0 +1,29 @@
+package com.simplekey.dto;
+
+public class Todo {
+
+    private String text;
+    private boolean done;
+
+    // 생성자
+    public Todo (String text, boolean done) {
+        this.text = text;
+        this.done = done;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
