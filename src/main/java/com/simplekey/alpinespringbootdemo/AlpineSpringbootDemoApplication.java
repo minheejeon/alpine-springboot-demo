633468ba0637ec6781d9b7a41d90e16f8919eb50
minheejeon <minhee.jeon@simplekey.co.kr>
2025-12-16 16:36:19 +0900

    Initial commit

---
+++ src/main/java/com/simplekey/alpinespringbootdemo/AlpineSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.simplekey.alpinespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpineSpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlpineSpringbootDemoApplication.class, args);
+    }
+
+}
